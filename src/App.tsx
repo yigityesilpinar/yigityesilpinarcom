f457c8d1b75c5c47a4890aa0946730b0dcf7fa14
Yigit Yesilpinar <yigityesilpinar@Yigits-MacBook-Pro-3.local>
2020-06-11 13:24:05 +0200

    Initial commit, Simple setup babel webpack typescript and react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { hot } from "react-hot-loader";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to my Personal Page</h1>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
