b18f4c79af17307a55d9cd2fcf01f8d8bf6736a4
Yigit Yesilpinar <yigityesilpinar@Yigits-MacBook-Pro-3.local>
2020-06-16 14:27:41 +0200

    Test pipeline and publish

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -5,10 +5,10 @@ import Techs from 'src/components/Techs'
 import { Container } from './styles'
 
 const Home: React.FC<unknown> = () => (
-    <Container>
-      <h1>Welcome to my Personal Pages</h1>
-      <Techs />
-    </Container>
-  )
+  <Container>
+    <h1>Welcome to App Latest Version</h1>
+    <Techs />
+  </Container>
+)
 
 export default Home
